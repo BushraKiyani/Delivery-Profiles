a12d984bfb11533336e3b1a2094e507357d0250f
Bushra Kiyani <bushra.kiyani@tu-dortmund.de>
2023-10-31 09:40:52 +0100

    updated.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from a_Variability import *
 from a_apply_profiles import *
 from a_adjust_Record import *
 from config import *
-from weekday_analysis_plot import *
+from weekday_analysis_plots import *
 
 if __name__ == "__main__":
 
     transport_preis = pd.read_csv(transport_price_path, encoding="latin_1", sep=";", decimal=',')
-    data_processed = pd.read_csv(df_distance_path, encoding="latin_1", sep=";", decimal=',')
+    data_processed = pd.read_csv(df_freightcost_path, encoding="latin_1", sep=";", decimal=',')
 
     # Check if 'latitude' and 'longitude' columns exist in the DataFrame
     if 'Empfänger_lon' in data_processed.columns and 'Empfänger_lat' in data_processed.columns\
@@ -25,15 +25,15 @@ if __name__ == "__main__":
                                                                  df_coordinates_path, json_coordinate_list_path)
 
     # Check if 'latitude' and 'longitude' columns exist in the DataFrame
-    #if 'Real_Distance' in data_processed.columns and 'Duration' in data_processed.columns\
-          #  and 'Euc_Distance' in data_processed.columns:
-       # print("Distances already exist in the data. Calculating freightcost...")
-       # df_added_distances = df_added_coordinates
-    #else:
-       # print("Distances do not exist in the data. Adding distances...")
-        # Calculate distance matrices
-    distances, durations, euclidean, distance_table, df_added_distances = \
-        create_distance_matrices2(df_added_coordinates, list_coordinates, sender_lon, sender_lat,distances_path_C,
+    if 'Real_Distance' in data_processed.columns and 'Duration' in data_processed.columns\
+            and 'Euc_Distance' in data_processed.columns:
+        print("Distances already exist in the data. Calculating freightcost...")
+        df_added_distances = df_added_coordinates
+    else:
+       print("Distances do not exist in the data. Adding distances...")
+       # Calculate distances
+       distances, durations, euclidean, distance_table, df_added_distances = \
+        create_distance_matrices(df_added_coordinates, list_coordinates, sender_lon, sender_lat,distances_path_C,
                                  duration_path_C,euk_distance_path_C,matrix_table_path_C,distances_path_S,
                                  duration_path_S, euk_distance_path_S, df_distance_path, chunk_size=100)
 
